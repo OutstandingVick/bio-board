882740f98dbbe178140fcc8af0f9743102d0ba1b
OutstandingVick <scottdodo2015@outlook.com>
2026-08-28 14:05:32 +0100

    fix route scroll effect cleanup

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import SectionPage from './SectionPage';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-  useEffect(() => window.scrollTo(0, 0), [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return null;
 }
 
